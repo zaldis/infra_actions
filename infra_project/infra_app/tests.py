202181ebc170b54e71df5178c46ae97d606bf4b3
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2022-01-14 07:48:55 +0200

    Fix tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -18,7 +18,7 @@ class StaticPagesURLTests(TestCase):
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
-        self.assertContains(response, 'У меня получилось!')
+        self.assertContains(response, 'У меня получилось настроить GitHub Actions!')
 
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
